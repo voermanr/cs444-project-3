648bf08f51a8a9c8789b9df69a58fe66f1ee9eae
Ross Voerman <voermanr@oregonstate.edu>
2023-04-21 14:18:52 -0700

    moving stuff around

--- eventbuf.c
+++ eventbuf.c
@@ -62,20 +62,3 @@ int eventbuf_empty(struct eventbuf *eb)
     return eb->head == NULL;
 }
 
-sem_t *sem_open_temp(const char *name, int value)
-{
-    sem_t *sem;
-
-    // Create the semaphore
-    if ((sem = sem_open(name, O_CREAT, 0600, value)) == SEM_FAILED)
-        return SEM_FAILED;
-
-    // Unlink it so it will go away after this process exits
-    if (sem_unlink(name) == -1) {
-        sem_close(sem);
-        return SEM_FAILED;
-    }
-
-    return sem;
-}
-
--- pcseml.c
+++ pcseml.c
@@ -1,6 +1,28 @@
 #include "pcseml.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <sys/semaphore.h>
+#include <fcntl.h>
+
+sem_t *mutex, *free_spots, *items;
+
+sem_t *sem_open_temp(const char *name, int value)
+{
+    sem_t *sem;
+
+    // Create the semaphore
+    if ((sem = sem_open(name, O_CREAT, 0600, value)) == SEM_FAILED)
+        return SEM_FAILED;
+
+    // Unlink it so it will go away after this process exits
+    if (sem_unlink(name) == -1) {
+        sem_close(sem);
+        return SEM_FAILED;
+    }
+
+    return sem;
+}
 
 int notify_consumers() {
     return 0;
