572b2d721df5b1f37e1860660ac667a828417851
Ross Voerman <voermanr@oregonstate.edu>
2023-04-21 15:41:49 -0700

    removed function declarations from header.

--- pcseml.h
+++ pcseml.h
@@ -3,12 +3,5 @@
 
 #include "eventbuf.h"
 
-int parse_command_line(char** arg, int *p, int *c, int *e, int *m);
-
-int start_producers(int producers);
-int start_consumers(int producers);
-int wait_for_producers();
-int notify_consumers();
-int wait_for_consumers();
 
 #endif
